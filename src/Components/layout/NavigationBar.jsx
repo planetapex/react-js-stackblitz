9051a84fb7723efd98acfdf6f052ed1264b06021
planetapex <yasirali.wizerp@gmail.com>
2023-04-10 03:23:12 +0400

    basic navigation complete

--- src/Components/layout/NavigationBar.jsx
+++ src/Components/layout/NavigationBar.jsx
@@ -12,32 +12,33 @@ const NavigationBar = () => {
 
   const activeKey = location.pathname === '/' ? '/about' : location.pathname;
 
-
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand as={Link} to="/" className="d-f1ex p-4 pb-0">
+        <Navbar.Brand as={Link} to="/" className="d-f1ex pb-0">
           <Image
             src="https://stackblitz.com/files/react-99avqn/github/planetapex/react-js-stackblitz/main/src/images.png"
             height="30"
-            className="aLign-top"
+            className="align-top"
             alt="Company Logo"
-          />
-          <p className="p-3 pt-0 pb-0 text-dark info">Reacto</p>
+          /> {' '}
+          Reacto
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+       
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav activeKey={activeKey} className="me-auto">
-            <Nav.Link as={Link} eventKey="/"  to="/">
+            <Nav.Link as={Link} eventKey="/" to="/">
               Home
             </Nav.Link>
-            <Nav.Link as={Link} eventKey="/blog"  to="/blog">
+            <Nav.Link as={Link} eventKey="/blog" to="/blog">
               Blog
             </Nav.Link>
-            <Nav.Link as={Link} eventKey="/about"  to="/about">
+            <Nav.Link as={Link} eventKey="/about" to="/about">
               About
             </Nav.Link>
-            <Nav.Link as={Link}  eventKey="/contact"  to="/contact">
+            <Nav.Link as={Link} eventKey="/contact" to="/contact">
               Contact
             </Nav.Link>
 
@@ -54,7 +55,7 @@ const NavigationBar = () => {
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
-      </Container>
+        </Container>
     </Navbar>
   );
 };
